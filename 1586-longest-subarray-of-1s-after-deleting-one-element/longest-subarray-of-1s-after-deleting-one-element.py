f331cabe4c994229367524c5736e00af00add45c
Praveen <37238720+impravin22@users.noreply.github.com>
2024-06-10 11:37:28 +0800

    Time: 462 ms (46.99%) | Memory: 19.5 MB (90.66%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -4,7 +4,6 @@ class Solution:
         left = 0
         max_length = 0 
         zero_count = 0
-    
         for right in range(len(nums)):
             if nums[right] == 0:
                 zero_count += 1
